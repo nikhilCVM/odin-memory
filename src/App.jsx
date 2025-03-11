9f157e5657e4bbefba4899a0b7bb6e7e9355e62a
Nikhil <manikantanikhil5@gmail.com>
2025-03-11 15:54:09 +0530

    mo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import ScoreBoard from "./components/ScoreBoard";
+
 import GameBoard from "./components/GameBoard";
 
 
 export default function App(){
-  const [score,setScore] = useState(0);
-  const [highScore,setHighScore] = useState(0);
+ 
   return(
     <>
     <div>
